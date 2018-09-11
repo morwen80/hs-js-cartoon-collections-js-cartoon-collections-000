8bd213b9759e69d3efeb9d19213fa9e512f6d9fe
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-11 21:46:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function dwarfRollCall(dwarves) {
+ let names = 1
+ return dwarves.map(x => names++ +". " + x + " ").join("");
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  return planeteerCalls.map(x => x.uppercase + "!")
 }
+
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+   if(words[i].length > 4) {
+      return true 
+   } else {
+ return false 
+   }
+  }
 }
 
+
 function findTheCheese (foods) {
+for(let i = 0; i < foods.length; i++) {
+  if (foods.includes("cheddar")){
+    return "cheddar"
+  } else if (foods.includes("gouda")) {
+    return "gouda"
+  } else if (foods.includes("camembert")) {
+    return "camembert"
+  } else {
+    return "no cheese!"
+  }
+}
+
 }
